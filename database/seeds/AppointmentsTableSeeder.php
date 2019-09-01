7bdfc3514f6dbaf42fa549301dbc6bc56d980b92
Fernando Gross <gross51@outlook.com>
2019-09-01 17:39:29 -0300

    Add a Seeder for new Appointments

---
+++ database/seeds/AppointmentsTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AppointmentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('appointments')->insert([
+			'start_date' => '2019-09-02',
+			'concluded_date' => null,
+			'deadline' => '2019-09-06',
+			'status' => 0,
+			'title' => 'Study about Laravel',
+			'description' => 'Cover basic CRUD and Auth.',
+			'user' => 'Fernando'
+        ]);
+
+        DB::table('appointments')->insert([
+			'start_date' => '2019-09-09',
+			'concluded_date' => null,
+			'deadline' => '2019-09-13',
+			'status' => 0,
+			'title' => 'Build Rest API',
+			'description' => 'Develop a Rest API using Laravel.',
+			'user' => 'Fernando'
+        ]);
+
+        DB::table('appointments')->insert([
+			'start_date' => '2019-09-16',
+			'concluded_date' => null,
+			'deadline' => '2019-09-20',
+			'status' => 0,
+			'title' => 'Build Tests for Rest API',
+			'description' => 'Cover every method of Appointments Service and Repository.',
+			'user' => 'Fernando'
+        ]);
+    }
+}
