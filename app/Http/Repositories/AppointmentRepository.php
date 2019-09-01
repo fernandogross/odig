51addcd6c0b2303c04bbd2dad0409f699cf15605
Fernando Gross <gross51@outlook.com>
2019-09-01 17:35:21 -0300

    Create a Controller, Service, Repository pattern
    This commit adds a Controller/Service/Repository pattern where Controller communicate with front, Service deal with business rules and Repository make all the interaction with database.

---
+++ app/Http/Repositories/AppointmentRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Repositories;
+
+use App\Appointment;
+
+class AppointmentRepository
+{
+	private $appointment;
+
+	public function __construct(Appointment $appointment)
+	{
+		$this->appointment = $appointment;
+	}
+
+	public function index()
+	{
+		return $this->appointment->get();
+	}	
+
+	public function store(array $data)
+	{
+		return $this->appointment->fill($data)->save();
+	}
+
+	public function update(array $data)
+	{
+		return $this->appointment->where('id', $data['id'])->update($data);
+	}
+
+	public function destroy($id)
+	{
+		return $this->appointment->find($id)->delete();
+	}
+
+	public function sort($fromDate, $toDate)
+	{
+		return $this->appointment->whereBetween('start_date', [$fromDate, $toDate])->get();
+	}
+
+	public function first($params)
+	{
+		return $this->appointment->where($params)->first();
+	}
+}
