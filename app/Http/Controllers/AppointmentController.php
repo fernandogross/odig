31ab5450758f386c676ce7b99601cee3e377ece3
Fernando Gross <marciogross@gmail.com>
2021-01-06 10:14:41 -0300

    Revert "Repository Pattern and index method improvements"
    This reverts commit cef86724da10f1ab6701d60a6be68fb34f58c457.

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -4,43 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Repositories\AppointmentRepository;
+use App\Http\Services\AppointmentService;
 
 class AppointmentController extends Controller
 {
-    private $appointmentRepository;
+    private $appointmentService;
 
-    public function __construct(AppointmentRepository $appointmentRepository)
+    public function __construct(AppointmentService $appointmentService)
     {
-        $this->appointmentRepository = $appointmentRepository;
+        $this->appointmentService = $appointmentService;
     }
 
-    public function index(Request $request)
+    public function index()
     {
-        $params = [
-            'fromDate' => $request->fromDate,
-            'toDate' => $request->toDate
-        ];
-        return response()->json($this->appointmentRepository->index($params));
+        return response()->json($this->appointmentService->index());
     }
 
     public function store(Request $request)
     {
         $data = $request->all();
-        $result = $this->appointmentRepository->store($data);
+        $result = $this->appointmentService->store($data);
         return response()->json($result, $result['status']);
     }
 
     public function update(Request $request)
     {
         $data = $request->all();
-        $result = $this->appointmentRepository->update($data);
-        return response()->json($result, $result['status']);
+        return $this->appointmentService->update($data);
     }
 
     public function destroy($id)
     {
-        $result = $this->appointmentRepository->destroy($id);
+        $result = $this->appointmentService->destroy($id);
+        return response()->json($result, $result['status']);
+    }
+
+    public function sort(Request $request)
+    {
+        $result = $this->appointmentService->sort($request->fromDate, $request->toDate);
         return response()->json($result, $result['status']);
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,6 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\User;
-use App\Appointment;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
